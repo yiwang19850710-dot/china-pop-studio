77bdb2e1365f829659a07de48ac49fe077803bcf
yiwang19850710-dot <yi.wang19850710@gmail.com>
2026-05-08 16:22:38 -0400

    Ensure hidden templates apply after app load

--- hidden-loader.js
+++ hidden-loader.js
@@ -1,4 +1,10 @@
 (() => {
+  if (window.CHINA_POP_HIDDEN_TEMPLATES?.apply) {
+    window.CHINA_POP_HIDDEN_TEMPLATES.install?.();
+    window.CHINA_POP_HIDDEN_TEMPLATES.apply();
+    return;
+  }
+
   const script = document.createElement("script");
   script.src = `hidden-templates.js?v=${Date.now()}`;
   script.async = false;
